134e224d1edbb1d1a3c5fdf4d8e500e050305c46
ronak <ronakgupta.official@gmail.com>
2023-07-05 23:06:38 +0530

    Start Learning Android Dev

---
+++ app/src/main/java/com/learnAndroid/myapplication/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.learnAndroid.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        println("On Create Called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("On Start Called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("On Resume Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("On Pause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("On Stop Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        println("On Restart Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("On Destroy Called")
+    }
+}
